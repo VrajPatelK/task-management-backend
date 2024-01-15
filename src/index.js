6d1fd3ca6c41b78a82a62bb56bd1d74e71d6d3a2
Vraj patel <vraj.patel4801@gmail.com>
2024-01-15 12:27:55 +0530

    cors middleware is added at server to accept request from another port(or. diff. server)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // pkgs
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import "colors";
 
 // routers
@@ -12,9 +13,18 @@ const app = express();
 const PORT = Number(process.env.SERVER_PORT);
 
 // middlewares
+app.use(
+  cors({
+    origin: process.env.REACT_APP_DOMAIN,
+  })
+);
 app.use(express.json());
 app.use("/api/v1/users/", user_routers);
 app.use("/api/v1/tasks/", task_routers);
+
+// app.get("/", (req, res) => {
+//   return res.status(200).json({ message: "hello from server" });
+// });
 
 // server
 app.listen(PORT, () => {
